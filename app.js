20c001f45d760ca30bbe0b86e2c1386cabd9fbbd
Julia Kristensen <julia.kristensen05@gmail.com>
2024-05-27 12:12:46 +0200

    f

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ window.addEventListener("load", initApp); // When the page is loaded, run initAp
 // Function to initialize the Web App
 function initApp() {
   console.log("initApp: app.js is running 🎉"); // Log to the console that the app is running
+  getPosts();
 }
 async function getPosts(){
   const response = await fetch("https://headless.cederdorff.dk/wp-json/wp/v2/posts?acf_format=standard ");
